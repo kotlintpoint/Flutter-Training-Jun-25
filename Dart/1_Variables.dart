bdd8c6cdba3552f7f09bc17379d4b58304c4db72
Kotlintpoint <ankit.sodha123@gmail.com>
2025-06-28 11:33:13 +0530

    Dart setup and Variable in Dart

---
+++ Dart/1_Variables.dart
@@ -0,0 +1,22 @@
+void main() {
+  print("Hello World");
+  int no1= 100;
+  int no2 =200;
+  int sum = no1 + no2;
+  print("Sum= $sum");
+
+  String name;
+
+  name = 'Voyager I';
+  // name = 100;
+
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg',
+  };
+
+  print("Length Of string : ${name.length}");
+}
