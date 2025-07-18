e52e055bc2f71b2b8b2917a78a921f6f7bbaec2c
Kotlintpoint <ankit.sodha123@gmail.com>
2025-07-18 10:52:49 +0530

    Reusable Stateless Widgets
    Text, Container, Center, Decoration, Color, constructor, Gradient.

---
+++ dice_app/lib/gradient_container.dart
@@ -0,0 +1,25 @@
+import 'package:dice_app/styled_text.dart';
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  @override
+  Widget build(BuildContext context) {
+    const topAlignment = Alignment.topLeft;
+    const bottomAlignment = Alignment.bottomRight;
+
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: topAlignment,
+          end: bottomAlignment,
+        ),
+      ),
+      child: Center(child: StyledText("Hello World!!!")),
+    );
+  }
+}
---
+++ dice_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:dice_app/gradient_container.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        // backgroundColor: Color.fromARGB(155, 6, 120, 60),
+        body: GradientContainer(
+          colors: [
+            Color.fromARGB(155, 150, 120, 60),
+            Color.fromARGB(150, 120, 120, 60),
+          ],
+        ),
+      ),
+    ),
+  );
+}
