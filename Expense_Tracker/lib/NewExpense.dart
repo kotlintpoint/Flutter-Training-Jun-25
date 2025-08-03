50e052b16033c677ac2e2da656bebff3e7f75ee7
Kotlintpoint <ankit.sodha123@gmail.com>
2025-08-03 10:56:28 -0700

    New Expense Form
    Date Picker, App Bar, Bottom Popup,  Navigator, TextField,

---
+++ Expense_Tracker/lib/NewExpense.dart
@@ -0,0 +1,106 @@
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  const NewExpense({super.key});
+
+  @override
+  State<NewExpense> createState() => _NewExpenseState();
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  // var _enteredTitle = "";
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  // void _saveTitleInput(String inputTitle) {
+  //   _enteredTitle = inputTitle;
+  // }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 50,
+            // onChanged: _saveTitleInput,
+            controller: _titleController,
+            decoration: InputDecoration(label: Text("Title")),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    prefixText: "\$ ",
+                    label: Text("Amount"),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? "No Date Selected"
+                          : formatter.format(_selectedDate!),
+                    ),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // print(_enteredTitle);
+                  print(_titleController.text);
+                },
+                child: Text("Save Expense"),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- Expense_Tracker/lib/expenses_screen.dart
+++ Expense_Tracker/lib/expenses_screen.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/NewExpense.dart';
 import 'package:expense_tracker/expenses_list/expense_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
@@ -25,9 +26,22 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter ExpenseTracker"),
+        actions: [
+          IconButton(onPressed: _openExpenseOverlay, icon: Icon(Icons.add)),
+        ],
+      ),
       body: Column(
         children: [
           Text("This is chart..."),
